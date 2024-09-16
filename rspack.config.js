469180639b20043d754fd7bbaecfb86b9ff7a3db
yodit93 <yoditabebe9391@gmail.com>
2024-09-16 18:18:12 +0300

    Used Rspack, dotenv for environmental variables managment

---
+++ rspack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { EnvironmentPlugin } = require('@rspack/core');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+module.exports = {
+  entry: './script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [new EnvironmentPlugin(['GEMINI_API_KEY'])]
+ 
+};
