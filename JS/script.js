26b3a7c69dee032d99d4fa01d1f26227891299c3
yodit93 <yoditabebe9391@gmail.com>
2024-10-12 17:40:29 +0300

    Update home page style as per the given style and content

--- JS/script.js
+++ JS/script.js
@@ -225,13 +225,6 @@ async function generateContent(userInput, userName, userLocation, userWebsite=''
     }
 }
 
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.fixed');
-    window.scrollY > 0 
-        ? header.classList.add('header-scrolled') 
-        : header.classList.remove('header-scrolled');
-});
-
 chatbotToggle.addEventListener('click', toggleChat);
 continueBtn.addEventListener('click', handleContinue);
 startBtn.addEventListener('click', handleStart);
